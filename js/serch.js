dcbe23e56660fb00daea8b667cee34ce20e9f4c0
Yassine Achouyne <yassine.achouyne2022@gmail.com>
2022-05-31 23:05:06 +0100

    31/5

--- js/serch.js
+++ js/serch.js
@@ -1,14 +1,10 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
       searchToggle = document.querySelector(".searchToggle"),
       sidebarOpen = document.querySelector(".sidebarOpen"),
       siderbarClose = document.querySelector(".siderbarClose");
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
+      
 
 // js code to toggle search box
         searchToggle.addEventListener("click" , () =>{
