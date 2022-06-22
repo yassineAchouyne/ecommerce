11e6aece3919e05e4c77485c79fd98a079b3e365
Yassine Achouyne <yassine.achouyne2022@gmail.com>
2022-06-22 17:14:02 +0100

    22/6

--- php/panair.php
+++ php/panair.php
@@ -28,7 +28,6 @@ if(isset($_GET['idSupprim'])){
             <td>Image de produit</td>
             <td>Nom de produit</td>
             <td>prix de produit</td>
-            <td>Quantity</td>
             <td>description de produit</td>
             <td>Action</td>
         </tr>
@@ -36,35 +35,22 @@ if(isset($_GET['idSupprim'])){
         $selectPaneir=$db->prepare("SELECT * FROM produit_panier");
         $selectPaneir->execute();
         $tablePanier=$selectPaneir->fetchAll();
+        $somme=0;
         foreach($tablePanier as $val){
-
+            
         ?>
         <tr>
             <td><img src="../admin/image/<?=$val['img_produit_panier'] ?>" alt=""></td>
             <td><?=$val['nom_produit_panier'] ?></td>
             <td class="prix_prd"><?=$val['prix_produit_panier'] ?> DH</td>
-            <td>
-                <input type="number" name="qt" class="qt"  value="1">
-            </td>
             <td><?=$val['dscription_produit_panier'] ?></td>
             <td><a href="panair.php?idSupprim=<?= $val['id_produit_panier']?>"><i class="fa-solid fa-xmark"></i></a></td>
         </tr>
-        <?php }?>
+        <?php $somme+=$val['prix_produit_panier'];}?>
     </table>
     <div>
-        <script>
-            var tab=document.getElementsByClassName('tablePanier')[0];
-            var tr=tab.getElementsByTagName('tr');
-            var somme=0;
-            for(i=1;i<tr.length;i++){
-                var pr=tr[i].getElementsByClassName('prix_prd')[0];
-                var qt=tr[i].getElementsByClassName('qt')[0];
-                somme+=Number( pr.innerHTML)*qt.value;
-            }
-            var rus=document.getElementById('total');
-            rus.innerHTML=somme;
-        </script>
-        <h3 >total <span style="color: #9e121b;" id="total"></span></h3>
+        <h3 >total <span style="color: #9e121b;" id="total"><?php echo $somme ." DH"?></span><a href="" class="Acheter"> Acheter</a></h3>
+        
     </div>
 </section>
 <?php include "../inc/footer.php" ?>
